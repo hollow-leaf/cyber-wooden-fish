e1acc8fa864df8d0c65febda88145c5f8d0ca689
Jarek Lin <s410673074@gmail.com>
2024-11-16 18:53:00 +0700

    feat: dyanmic wallet network

--- apps/pwa/src/components/providers/wallet-provider.tsx
+++ apps/pwa/src/components/providers/wallet-provider.tsx
@@ -3,19 +3,21 @@ import {
   DynamicWidget,
 } from "@dynamic-labs/sdk-react-core";
 import { http } from "viem";
-import { mainnet } from "viem/chains";
+import { baseSepolia, scrollSepolia, arbitrumSepolia } from "viem/chains";
 import { PropsWithChildren } from "react";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { FlowWalletConnectors } from "@dynamic-labs/flow";
 import { createConfig, WagmiProvider } from "wagmi";
 
 const dynamicKey = import.meta.env.VITE_DYNAMICAPIKEY;
 
 const config = createConfig({
-  chains: [mainnet],
-  multiInjectedProviderDiscovery: false,
+  chains: [baseSepolia, scrollSepolia, arbitrumSepolia],
   transports: {
-    [mainnet.id]: http(),
+    [baseSepolia.id]: http(),
+    [scrollSepolia.id]: http(),
+    [arbitrumSepolia.id]: http(),
   },
 });
 
@@ -23,10 +25,9 @@ export default function WalletProvider({ children }: PropsWithChildren) {
   return (
     <DynamicContextProvider
       settings={{
-        // Find your environment id at https://app.dynamic.xyz/dashboard/developer
         environmentId: dynamicKey,
 
-        walletConnectors: [EthereumWalletConnectors],
+        walletConnectors: [FlowWalletConnectors, EthereumWalletConnectors],
       }}
     >
       <WagmiProvider config={config}>
